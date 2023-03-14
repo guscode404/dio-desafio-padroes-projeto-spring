16626707001c616856d8adbb63cb5da7cc5bbdf4
Gustavo <gustavokingu0@gmail.com>
2023-03-13 23:09:39 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/gof/doc/SwaggerConfig.java
@@ -0,0 +1,68 @@
+/*package one.digitalinnovation.gof.doc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.service.Contact;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    private Contact contato() {
+        return new Contact(
+                "Seu nome",
+                "https://www.seusite.com.br",
+                "voce@seusite.com.br");
+    }
+
+    private ApiInfoBuilder informacoesApi() {
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+
+        apiInfoBuilder.title("Title - Rest API");
+        apiInfoBuilder.description("API exemplo de uso de Springboot REST API");
+        apiInfoBuilder.version("1.0");
+        apiInfoBuilder.termsOfServiceUrl("Termo de uso: Open Source");
+        apiInfoBuilder.license("Licença - Sua Empresa");
+        apiInfoBuilder.licenseUrl("https://seusite.com.br");
+        apiInfoBuilder.contact(this.contato());
+
+        return apiInfoBuilder;
+    }
+
+    @Bean
+    public Docket detalheApi() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+
+        docket
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("one.digitalinnovation.gof.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.informacoesApi().build())
+                .consumes(new HashSet<String>(Arrays.asList("application/json")))
+                .produces(new HashSet<String>(Arrays.asList("application/json")));
+
+        return docket;
+    }
+
+    @Override
+    public void addResourceHandlers (ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.
+                addResourceHandler("/webjars/")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
+*/
